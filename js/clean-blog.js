a51155752047dd75e18ce2f3580ece3e646788ca
Mike Vormwald <mvormwald@gmail.com>
2015-02-04 00:41:57 -0500

    Setup stylesheets & run grunt to compile

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,6 +1,6 @@
 /*!
  * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2015 Start Bootstrap
+ * Copyright 2014 Start Bootstrap
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
@@ -997,24 +997,8 @@ $('#name').focus(function() {
 
 })(jQuery);
 
-// make all images responsive
-$(function() {
-	$("img").addClass("img-responsive");
-});
-
-// responsive tables
-$(document).ready(function() {
-	$("table").wrap("<div class='table-responsive'></div>");
-	$("table").addClass("table");
-});
-
-// responsive embed videos
-$(document).ready(function () { 
-    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
-    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
-	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
-});
+//make all images responsive
+$("img").addClass("img-responsive")
 
 // Floating label headings for the contact form
 $(function() {
